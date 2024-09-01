3dc8cbf15f8183344390d66534317914d2c78594
baljeetp <baljeetprajapati38@gmail.com>
2024-09-01 10:36:01 +0530

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,19 @@ const cors=require("cors");
 const connectMongoDB= require("./connection/conn");
 const auth=require("./routes/auth");
 const list=require("./routes/list");
-
+const path=require("path");
 connectMongoDB();
 
 app.use(express.json());
 
 app.use(cors());
-app.get("/",(req,res)=>{
-    res.send("hello");
-});
+app.get("/", (req, res) => {
+    app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    });
+// app.get("/",(req,res)=>{
+//     res.send("hello");
+// });
 
 app.use("/api/v1",auth);
 app.use("/api/v2",list);
