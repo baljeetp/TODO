1b27f35a561ea87e9b4dbf4eed1f4b65689bd65d
baljeetp <baljeetprajapati38@gmail.com>
2024-09-01 11:21:47 +0530

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,11 +10,11 @@ connectMongoDB();
 app.use(express.json());
 
 app.use(cors());
-app.use(express.static(path.resolve(__dirname, "/frontend/public")));
-app.get("*", (req, res) => {
+// app.use(express.static(path.resolve(__dirname, "/frontend/build")));
+// app.get("*", (req, res) => {
     
-    res.sendFile(path.resolve(__dirname, "/frontend/public/index.html"));
-    });
+//     res.sendFile(path.resolve(__dirname, "/frontend/build/index.html"));
+//     });
 // app.get("/",(req,res)=>{
 //     res.send("hello");
 // });
