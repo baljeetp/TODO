d5dbd4ed0c0a068905c6b7b70ce00cd92a3511fb
baljeetp <baljeetprajapati38@gmail.com>
2024-09-01 10:49:33 +0530

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
     app.use(express.static(path.resolve(__dirname, "/frontend")));
-    res.sendFile(path.resolve(__dirname, "/frontend/index.html"));
+    res.sendFile(path.resolve(__dirname, "/frontend/public/index.html"));
     });
 // app.get("/",(req,res)=>{
 //     res.send("hello");
