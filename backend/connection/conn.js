3ba7d1a70a613568650c99000e43d9ead5b5cb0e
baljeetp <99162607+baljeetp@users.noreply.github.com>
2024-09-02 10:43:09 +0530

    Update conn.js

--- backend/connection/conn.js
+++ backend/connection/conn.js
@@ -4,12 +4,11 @@ const conn = async (req, res) => {
   try {
     await mongoose.connect(
       "mongodb+srv://baljeetprajapati38:Avi12345@cluster0.10ofb.mongodb.net/",
-      { useNewUrlParser: true, useUnifiedTopology: true }
+      { useNewUrlParser: true}
     );
     console.log("Database connected successfully");
   } catch (error) {
     console.error("Database connection error:", error);
-    res.status(400).json({ message: "Database connection failed" });
   }
 };
 
